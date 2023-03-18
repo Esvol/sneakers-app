e3dd752fc9909d2f1cfcf5ba33370ac437179dab
Esvol <o.voloshyn@obdarovanist.kh.ua>
2023-03-18 18:16:29 +0100

    'changes2'

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+const Header = () => {
+return(
+    <header className='d-flex justify-between align-center p-40'>
+        <div className='d-flex align-center'>
+            <img width={40} height={40} alt='' src='/img/logo.png'/>
+            <div className='headerInfo'>
+                <h3 className='text-uppercase'>React sneakers</h3>
+                <p className='opacity-5'>Магазин лучших кроссовок</p>
+            </div>
+        </div>
+        <ul className='d-flex'>
+            <li className='mr-30'>
+                <img width={18} height={18} alt='' src='/img/cart.svg'/>
+                <span>1205 грн</span>
+            </li>
+            <li>
+                <img width={18} height={18} alt='' src='/img/user.svg'/>
+            </li>
+        </ul>
+    </header>
+);
+}
+
+export default Header;
